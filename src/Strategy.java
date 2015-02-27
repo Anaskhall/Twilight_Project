9d15b4c7f502c53e64e986d82fa9dfceadedaa82
Anas Khallouf <anas-kh@hotmail.fr>
2015-02-27 11:38:35 +0100

    Start strategy class

--- src/Strategy.java
+++ src/Strategy.java
@@ -1,11 +1,28 @@
 import models.Map;
+import models.Monster;
+import models.Move;
+
+import java.util.ArrayList;
 
 /**
  * Created by Anas-kh on 26/02/2015.
  */
 public class Strategy {
 
-    public void chooseMove( Map map){
+    public Move chooseMoveMonster(Map map, Monster monster){
+        Move move = null;
+        return move;
+    }
 
+    public ArrayList<Move> chooseMoves(Map map){
+        ArrayList<Move> moves = new ArrayList<Move>();
+        for (Monster monster : map.getArmy()){
+            Move move = chooseMoveMonster(map, monster);
+            if (move!=null){
+                moves.add(move);
+            }
+
+        }
+        return moves;
     }
 }
