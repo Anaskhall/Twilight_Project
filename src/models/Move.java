9112c9f0d743108e0ed32784a86a88b771a84552
Anas Khallouf <anas-kh@hotmail.fr>
2015-02-27 11:38:05 +0100

    Create move object

--- src/models/Move.java
+++ src/models/Move.java
@@ -5,9 +5,57 @@ package models;
  */
 public class Move {
 
-    private int start_x;
-    private int start_y;
+    private int startX;
+    private int startY;
     private int number;
-    private int finish_x;
-    private int finish_y;
+    private int finishX;
+    private int finishY;
+
+    public Move(int startX, int startY, int number, int finishX, int finishY) {
+        this.startX = startX;
+        this.startY = startY;
+        this.number = number;
+        this.finishX = finishX;
+        this.finishY = finishY;
+    }
+
+    public int getStartX() {
+        return startX;
+    }
+
+    public void setStartX(int startX) {
+        this.startX = startX;
+    }
+
+    public int getStartY() {
+        return startY;
+    }
+
+    public void setStartY(int startY) {
+        this.startY = startY;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getFinishX() {
+        return finishX;
+    }
+
+    public void setFinishX(int finishX) {
+        this.finishX = finishX;
+    }
+
+    public int getFinishY() {
+        return finishY;
+    }
+
+    public void setFinishY(int finishY) {
+        this.finishY = finishY;
+    }
 }
