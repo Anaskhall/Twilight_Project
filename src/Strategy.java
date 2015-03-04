4d6b31da592d339a9eea34d7e82a2e71cdcbc434
sileod <damien.sileo@gmail.com>
2015-03-04 23:15:39 +0100

    Added minmax frame

--- src/Strategy.java
+++ src/Strategy.java
@@ -28,35 +28,36 @@ public class Strategy {
 
     public boolean currentPlayer;
 
-    public float rate(Map map){
+    public float rating(Map map){
         return 0;// TODO
     }
 
-    public List<Map> possibleMaps(){
+    public List<Map> possibleMaps(Map map){
         List maps = new ArrayList<Map>();
         return maps; //TODO
     }
 
-    public float alphaBeta(Map map, int depth, float alpha, float beta) {
+    public float alphaBeta(Map map, int depth, float alpha, float beta, boolean currentPlayer) {
 
         if (depth <= 0) {
-            return rate(map);
+            return rating(map); //rate a map
         }
 
-        List<Map> children = possibleMaps(); // generates children. also rates them and applies move to copy of field.
+        List<Map> children = possibleMaps(map); // generates list of possible maps
 
         if (currentPlayer) { // ai tries to maximize the score
             for (Map child : children) {
-                alpha = Math.max(alpha, alphaBeta(child, depth - 1, alpha, beta));
+                alpha = Math.max(alpha, alphaBeta(child, depth - 1, alpha, beta, !currentPlayer));
 
                 if (beta <= alpha) {
                     break; // cutoff
                 }
             }
             return alpha;
+
         } else { // enemy tries to minimize the score
             for (Map child : children) {
-                beta = Math.min(beta, alphaBeta(map, depth - 1, alpha, beta));
+                beta = Math.min(beta, alphaBeta(map, depth - 1, alpha, beta, !currentPlayer));
                 if (beta <= alpha) {
                     break; // cutoff
                 }
