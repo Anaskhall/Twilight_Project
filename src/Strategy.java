e80bc37d20e7793dc7fea7175e6597e324f7e339
sileod <damien.sileo@gmail.com>
2015-03-04 22:55:25 +0100

    Added minmax frame

--- src/Strategy.java
+++ src/Strategy.java
@@ -2,7 +2,7 @@ import models.Map;
 import models.Monster;
 import models.Move;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by Anas-kh on 26/02/2015.
@@ -25,4 +25,44 @@ public class Strategy {
         }
         return moves;
     }
+
+    public boolean currentPlayer;
+
+    public float rate(Map map){
+        return 0;// TODO
+    }
+
+    public List<Map> possibleMaps(){
+        List maps = new ArrayList<Map>();
+        return maps; //TODO
+    }
+
+    public float alphaBeta(Map map, int depth, float alpha, float beta) {
+
+        if (depth <= 0) {
+            return rate(map);
+        }
+
+        List<Map> children = possibleMaps(); // generates children. also rates them and applies move to copy of field.
+
+        if (currentPlayer) { // ai tries to maximize the score
+            for (Map child : children) {
+                alpha = Math.max(alpha, alphaBeta(child, depth - 1, alpha, beta));
+
+                if (beta <= alpha) {
+                    break; // cutoff
+                }
+            }
+            return alpha;
+        } else { // enemy tries to minimize the score
+            for (Map child : children) {
+                beta = Math.min(beta, alphaBeta(map, depth - 1, alpha, beta));
+                if (beta <= alpha) {
+                    break; // cutoff
+                }
+            }
+            return beta;
+        }
+    }
+    
 }
